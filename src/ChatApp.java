fdcaaceefa8a8ef8c43f4ccab61a1543744078ba
Bozhidar Manev <bojomanev@gmail.com>
2022-11-16 13:13:11 +0100

    use case diagram

---
+++ src/ChatApp.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
---
+++ src/RunChatApp.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+
+public class RunChatApp {
+    public static void main(String[] args) {
+        Application.launch(ChatApp.class);
+    }
+}
