bc36f3a0deca5860672c33cfc5c08c69af7eaf78
Bozhidar Manev <bojomanev@gmail.com>
2022-12-09 20:02:04 +0100

    DAO merge

--- code/src/dataBase/DAO.java
+++ code/src/dataBase/DAO.java
@@ -1,7 +1,8 @@
 package dataBase;
 
+import client.model.Chat;
 import server.model.User;
-import shared.Subject;
+import shared.util.Message;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -16,4 +17,19 @@ public interface DAO{
     String updatePassword(String username,String password) throws SQLException;
     String updateLastName(String username,String lastName) throws SQLException;
     String updateFirstName(String username,String firstName) throws SQLException;
+    List<Chat> requestChats(String username) throws SQLException;
+
+    public Chat createChat(String name) throws SQLException;
+
+    Message createMessage(Message message) throws SQLException;
+    List<Message> requestData(String user);
+
+    List<Message> loadMessages(String username);
+
+    void addMessagesToChats(Message message) throws SQLException;
+    void addReceiverToChat(String username, int chat) throws SQLException;
+    void removeReceiverFromChat(String username, int chatID) throws SQLException;
+
+    User create() throws SQLException;
+    User findUser(String user) throws SQLException;
 }
--- code/src/dataBase/DAOImpl.java
+++ code/src/dataBase/DAOImpl.java
@@ -1,7 +1,8 @@
 package dataBase;
 
-import com.sun.source.tree.BreakTree;
+import client.model.Chat;
 import server.model.User;
+import shared.util.Message;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -96,5 +97,118 @@ public class DAOImpl implements DAO {
             statement.executeUpdate();
             return firstName;
         }
+    }
+
+    public List<Chat> requestChats(String username) {
+        //TODO
+        return null;
+    }
+
+    public Chat createChat(String name) {
+        //TODO
+        return null;
+    }
+
+    @Override public Message createMessage(Message message)
+            throws SQLException
+    {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO messages(sender,toChat,text,datetime) VALUES (?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, message.getSender());
+            statement.setInt(2, message.getToChat());
+            statement.setString(3, message.getMessageBody());
+            statement.setString(4, message.dateTime());
+            statement.execute();
+        }
+        return new Message(message.getSender(), message.getToChat(), message.getMessageBody());
+    }
+
+    @Override public List<Message> requestData(String user)
+    {
+        return null;
+    }
+
+    @Override
+    public List<Message> loadMessages(String username) {
+//TODO
+        return null;
+    }
+
+
+    @Override public void addMessagesToChats(Message message) throws SQLException
+    {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO chats(id) VALUES (?)");
+            statement.setInt(1, message.getToChat());
+            statement.execute();
+            //needs to be worked on
+        }
+
+    }
+
+    @Override public void addReceiverToChat(String username, int chat)
+            throws SQLException
+    {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO receivers(username,chat) VALUES (?,?)");
+            statement.setString(1, username);
+            statement.setInt(2, chat);
+            statement.execute();
+        }
+
+    }
+
+    @Override public void removeReceiverFromChat(String username, int chatID)
+            throws SQLException
+    {
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM receivers(username,chat) VALUES (?,?)");
+            statement.setString(1, username);
+            statement.setInt(2, chatID);
+            statement.executeUpdate();
+        }
+    }
+
+    @Override
+    public User create() throws SQLException {
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO users(firstName,lastName,password,username) VALUES (?,?,?,?);");
+            statement.setString(1, "Cristiano");
+            statement.setString(2, "Ronaldo");
+            statement.setString(3, "SIU");
+            statement.setString(4, "CR7");
+            statement.execute();
+        }
+        return new User("CR7", "Cristiano", "Ronaldo", "SIU");
+    }
+
+    @Override
+    public User findUser(String name) throws SQLException {
+        User user = null;
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username=?;");
+            statement.setString(1, name);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String firstName = resultSet.getNString("firstName");
+                String lastName = resultSet.getNString("lastName");
+                String username = resultSet.getNString("username");
+                String password = resultSet.getNString("password");
+
+                user = new User(firstName, lastName, username, password);
+            }
+        }
+        return user;
     }
 }
