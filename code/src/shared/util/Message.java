a1e9e2aaafffb2d0bcbdaf04053d7914100eb7b4
AmeyaMahankal <ameya.mahankal@gmail.com>
2022-12-15 10:36:52 +0100

    UpdateSwear
    Made a static method which runs in message class

--- code/src/shared/util/Message.java
+++ code/src/shared/util/Message.java
@@ -4,47 +4,57 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Message implements Serializable {
-    private String sender;
-    private String messageBody;
-    private int toChat;
-    private String datetime;
+public class Message implements Serializable
+{
+  private String sender;
+  private String messageBody;
+  private int toChat;
+  private String datetime;
 
-    public Message(String sender, int toChat, String messageBody) {
-        this(sender, toChat, messageBody, new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+  public Message(String sender, int toChat, String messageBody)
+  {
+    this(sender, toChat, SwearDetection.changeSwearMapStatic(messageBody),
+        new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+  }
+
+  public Message(String sender, int toChat, String messageBody, String datetime)
+  {
+    this.sender = sender;
+    this.messageBody = SwearDetection.changeSwearMapStatic(messageBody);
+    this.toChat = toChat;
+    this.datetime = datetime;
+
+  }
+
+  public String getMessageBody()
+  {
+    return messageBody;
+  }
+
+  public String getSender()
+  {
+    return sender;
+  }
+
+  public int getToChat()
+  {
+    return toChat;
+  }
+
+  public String getDateTime()
+  {
+    return datetime;
+  }
+
+  @Override public String toString()
+  {
+    if (sender.equals("server"))
+    {
+      return String.format("%s", messageBody);
     }
-
-    public Message(String sender, int toChat, String messageBody, String datetime) {
-        this.sender = sender;
-        this.messageBody = messageBody;
-        this.toChat = toChat;
-        this.datetime = datetime;
-
-
+    else
+    {
+      return String.format("%s -> %s", sender, messageBody);
     }
-
-    public String getMessageBody() {
-        return messageBody;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public int getToChat() {
-        return toChat;
-    }
-
-    public String getDateTime() {
-        return datetime;
-    }
-
-    @Override
-    public String toString() {
-        if (sender.equals("server")) {
-            return String.format("%s", messageBody);
-        } else {
-            return String.format("%s -> %s", sender, messageBody);
-        }
-    }
+  }
 }
